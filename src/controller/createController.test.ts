a154dc954734be7728ba426a0284e704a3c5a9a0
Martini <me.martiinii@gmail.com>
2023-02-19 22:00:38 +0100

    run prettier

--- src/controller/createController.test.ts
+++ src/controller/createController.test.ts
@@ -7,31 +7,31 @@ jest.mock('./fetcher');
 const mockedFetcher = jest.mocked(fetcher);
 
 beforeEach(() => {
-    mockedFetcher.mockClear();
+	mockedFetcher.mockClear();
 });
 
 describe('Controller module', () => {
-    type User = {
-        name: string,
-        age: number
-    }
+	type User = {
+		name: string;
+		age: number;
+	};
 
-    const UserController = createController<User, CRUDBase, CRUDBase>({
-        $url: 'user',
-    })(crudTemplate);
+	const UserController = createController<User, CRUDBase, CRUDBase>({
+		$url: 'user',
+	})(crudTemplate);
 
-    test('Check default values', () => {
-        expect(UserController.$url).toBe('user');
-        expect(UserController.$base).toBe('custom');
-        expect(UserController.$protected).toBe(true);
-    });
+	test('Check default values', () => {
+		expect(UserController.$url).toBe('user');
+		expect(UserController.$base).toBe('custom');
+		expect(UserController.$protected).toBe(true);
+	});
 
-    test('Check if fetcher has been called', () => {
-        UserController.index();
-        expect(fetcher).toHaveBeenLastCalledWith('user', 'GET');
+	test('Check if fetcher has been called', () => {
+		UserController.index();
+		expect(fetcher).toHaveBeenLastCalledWith('user', 'GET');
 
-        UserController.read({ id: 15 });
-        expect(fetcher).toHaveBeenLastCalledWith('user', 'GET', { id: 15 });
-        expect(fetcher).toHaveBeenCalledTimes(2);
-    });
+		UserController.read({ id: 15 });
+		expect(fetcher).toHaveBeenLastCalledWith('user', 'GET', { id: 15 });
+		expect(fetcher).toHaveBeenCalledTimes(2);
+	});
 });
