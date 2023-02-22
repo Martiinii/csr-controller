e13cf277789bd762d9abb4839c77900acfe9b4a5
Martini <me.martiinii@gmail.com>
2023-02-22 19:42:35 +0100

    Created

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "import"
+    ],
+    "rules": {
+        "import/no-cycle": [
+            "error"
+        ],
+        "import/no-self-import": [
+            "error"
+        ]
+    }
+}
