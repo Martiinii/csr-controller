760c685c3343425a373222e9736bbde35e7257d2
Martini <me.martiinii@gmail.com>
2023-03-01 14:44:28 +0100

    Fixed fetcher GET request

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,37 @@
-import { CRUDFetchMethod } from '../';
+import { ControllerProps, CRUDBase, CRUDFetchMethod } from '../';
 
 /**
  * Simple fetcher
  *
- * @param resource API path
+ * @param c Controller properties
  * @param method CRUD method, e.g. GET or POST
  * @param data Optional object with data
+ * @param server Optional server parameter to use with shared controllers between projects
  * @returns Promise with result from fetch
+ *
+ * @todo implement server parameter logic
  */
 export const fetcher = <T>(
-	resource: string,
+	c: ControllerProps,
 	method: (typeof CRUDFetchMethod)[number],
-	data?: object | string
+	data?: (object & CRUDBase) | string,
+	server?: string
 ): Promise<T> => {
-	return fetch(typeof data === 'string' ? new URL(data, resource) : resource, {
+	const dataId = `${typeof data === 'string' ? data : data != null ? data.id : ''}`;
+
+	if (typeof data === 'object' && method == 'GET') {
+		delete data.id;
+		if (Object.keys(data).length == 0) data = undefined;
+	}
+
+	const url =
+		`/api/` +
+		(c.$base ? `${c.$base}/` : '') +
+		(c.$parentUrl ? `${c.$parentUrl}/${dataId}/${c.$url}` : `${c.$url}/${dataId}`) +
+		(method == 'GET' && data ? '?' + new URLSearchParams(data as Record<string, string>) : '');
+
+	return fetch(url, {
 		method,
-		body: JSON.stringify(typeof data === 'string' ? null : data),
+		body: method !== 'GET' ? JSON.stringify(typeof data === 'string' ? null : data) : null,
 	}).then(res => res.json()) as Promise<T>;
 };
