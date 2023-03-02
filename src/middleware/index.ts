c4b56a1c7ba97f064304791e97ea3c2a21019433
Martini <me.martiinii@gmail.com>
2023-03-02 17:09:44 +0100

    Added req and res to middlewares

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,11 @@
 import { APIFullErrorResponse } from '../apiRoutes/response';
 import { Controller, CRUDBase } from '../';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export type ControllerMiddleware = (
-	c: Controller<unknown, CRUDBase, CRUDBase>
+    c: Controller<unknown, CRUDBase, CRUDBase>,
+    req: NextApiRequest,
+    res: NextApiResponse
 ) => APIFullErrorResponse | undefined | Promise<APIFullErrorResponse | undefined>;
 
 export * from './debugMiddleware';
