2f9c4372307bd030f1ad8dc5aa838368524044dc
Martini <me.martiinii@gmail.com>
2023-03-15 20:23:32 +0100

    Added type to `register` function

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,17 +1,24 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { BaseControllerMethods, Controller, ControllerMethods, ControllerProps, CRUDBase, SubController } from '../';
-
-type PartialParameter<T extends (...args: unknown[]) => unknown> = Partial<Parameters<T>[0]> extends infer R
-	? R extends undefined
-		? never
-		: R
-	: never;
-type PartialFunction<T extends (...args: unknown[]) => unknown> = PartialParameter<T> extends infer R
-	? (arg0: R) => ReturnType<T>
-	: never;
 
 type PickByType<T, Value> = {
 	[P in keyof T as T[P] extends Value | undefined ? P : never]: T[P];
+};
+
+type RegisterFunction = <CONT extends Controller<unknown, never, never>>(
+	controller: CONT,
+	db: {
+		[method in keyof ControllerMethods<unknown, never, never>]?: CONT[method];
+	} & {
+		[sk in keyof PickByType<
+			Omit<CONT, keyof ControllerMethods<unknown, never, never> | keyof ControllerProps>,
+			ReturnType<SubController<never>>
+		>]: {
+			[k in keyof CONT[sk] & keyof BaseControllerMethods<unknown>]?: CONT[sk][k];
+		};
+	}
+) => {
+	register: RegisterFunction;
+	handle: () => Map<string, Controller<unknown, unknown, CRUDBase>>;
 };
 
 /**
@@ -20,28 +27,17 @@ type PickByType<T, Value> = {
  * @returns Object with register and handle methods
  */
 export const controllerRegistry = () => {
-	const routes = new Map<string, Controller<unknown, CRUDBase, CRUDBase>>();
+	const routes = new Map<string, Controller<unknown, unknown, CRUDBase>>();
 
 	/**
 	 * Register controller with database handlers
 	 *
-	 * @param c @see {@link Controller}
+	 * @param controller
 	 * @param db Database handlers for controller and sub controllers
-	 * @returns
+	 *
+	 *  @see {@link Controller}
 	 */
-	const register = <CONT extends Controller<unknown, CRUDBase, CRUDBase>>(
-		controller: CONT,
-		db: {
-			[method in keyof ControllerMethods<unknown, CRUDBase, CRUDBase>]?: PartialFunction<CONT[method]>;
-		} & {
-			[sk in keyof PickByType<
-				Omit<CONT, keyof ControllerMethods<unknown, CRUDBase, CRUDBase> | keyof ControllerProps>,
-				ReturnType<SubController<unknown>>
-			>]: {
-				[k in keyof CONT[sk] & keyof BaseControllerMethods<unknown>]?: CONT[sk][k];
-			};
-		}
-	) => {
+	const register: RegisterFunction = (controller, db) => {
 		const { index, create, destroy, read, update, ...rest } = db;
 		// Create copy of controller so we won't delete original keys later
 		const copy = { ...controller };
