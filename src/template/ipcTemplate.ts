7437a467e424c71d43554a08dfc3ac26becaa54a
Martini <me.martiinii@gmail.com>
2023-03-15 20:18:24 +0100

    prettier run

--- src/template/ipcTemplate.ts
+++ src/template/ipcTemplate.ts
@@ -3,13 +3,13 @@ import { IpcRenderer } from 'electron';
 import { ipcInvoker } from '../utils';
 
 export const ipcTemplate = (i: IpcRenderer) =>
-    createTemplate(c => ({
-        create: data => ipcInvoker(i, c, 'POST', data),
+	createTemplate(c => ({
+		create: data => ipcInvoker(i, c, 'POST', data),
 
-        index: () => ipcInvoker(i, c, 'GET'),
-        read: data => ipcInvoker(i, c, 'GET', data),
+		index: () => ipcInvoker(i, c, 'GET'),
+		read: data => ipcInvoker(i, c, 'GET', data),
 
-        update: data => ipcInvoker(i, c, 'PATCH', data),
+		update: data => ipcInvoker(i, c, 'PATCH', data),
 
-        destroy: data => ipcInvoker(i, c, 'DELETE', data),
-    }));
+		destroy: data => ipcInvoker(i, c, 'DELETE', data),
+	}));
